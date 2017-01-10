cf2a61d8aba91d23e6663bd8b1a535726872f1d4
iratierana <irati.erana@alumni.mondragon.edu>
2017-01-10 16:51:59 +0100

    Confifurations commented

--- src/configurations/ConnectHibernate.java
+++ src/configurations/ConnectHibernate.java
@@ -4,35 +4,69 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+/**
+ * The Class ConnectHibernate.
+ */
 public class ConnectHibernate {
 	
+	/** The session factory. */
 	private static SessionFactory sessionFactory;
+	
+	/** The session. */
 	private static Session session;
+	
+	/** The Constant CONFIG_FILE. */
 	private final static String CONFIG_FILE = "configurations/hibernate.cfg.xml";
 
 	
+	/**
+	 * Before.
+	 */
 	public static void before() {
 		sessionFactory = new Configuration().configure(CONFIG_FILE).buildSessionFactory();
 		session = sessionFactory.openSession();
 	}
 	
+	/**
+	 * After.
+	 */
 	public static void after() {
 		session.close();
 		sessionFactory.close();
 	}
 	
+	/**
+	 * Gets the session factory.
+	 *
+	 * @return the session factory
+	 */
 	public static SessionFactory getSessionFactory() {
 		return sessionFactory;
 	}
 	
+	/**
+	 * Sets the session factory.
+	 *
+	 * @param sessionFactory the new session factory
+	 */
 	public static void setSessionFactory(SessionFactory sessionFactory) {
 		ConnectHibernate.sessionFactory = sessionFactory;
 	}
 	
+	/**
+	 * Gets the session.
+	 *
+	 * @return the session
+	 */
 	public static Session getSession(){
 		return session;
 	}
 	
+	/**
+	 * Sets the session.
+	 *
+	 * @param session the new session
+	 */
 	public static void setSession(Session session) {
 		ConnectHibernate.session = session;
 	}
